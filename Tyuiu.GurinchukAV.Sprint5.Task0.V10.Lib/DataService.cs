dbabe4c920bd830a98579bb044da35739cdd0e1f
Анастасия Гуринчук <gurincuknasta59@gmail.com>
2025-11-19 23:11:15 +0300

    Add project files.

---
+++ Tyuiu.GurinchukAV.Sprint5.Task0.V10.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.GurinchukAV.Sprint5.Task0.V10.Lib
+{
+    public class DataService : ISprint5Task0V10
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
+            double z = 1.6 * Math.Pow(x, 3) - 2.1 * Math.Pow(x, 2) + 7 * x;
+            z = Math.Round(z, 3);
+            File.WriteAllText(path, Convert.ToString(z));
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.GurinchukAV.Sprint5.Task0.V10/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.GurinchukAV.Sprint5.Task0.V10.Lib;
+namespace Tyuiu.GurinchukAV.Sprint5.Task0.V10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил: Гуринчук А. В. | ИБКСб 25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #10                                                             *");
+            Console.WriteLine("* Выполнил: Гуринчук Анастасия Витальевна | ИБКСб 25-1                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дано выражение F(x) = 1.6x^3 - 2.1x^2 + 7x вычислить его значение       *");
+            Console.WriteLine("* при x = 2, результат сохранить в текстовый файл OutPutFileTask0.txt     *");
+            Console.WriteLine("* и вывести на консоль. Округлить до трёх знаков после запятой.           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x = 2;
+            Console.WriteLine("x = " + x);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(x);
+
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+
+            string fileContent = File.ReadAllText(res);
+            Console.WriteLine("Значение выражения: " + fileContent);
+
+            Console.ReadKey();
+        }
+    }
+}
